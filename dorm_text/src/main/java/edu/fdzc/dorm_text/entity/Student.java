962b0519826e57a7d47ac6e3849b18119c1caba6
wym2468 <3380944865@qq.com>
2025-06-19 11:28:04 +0800

    Add files via upload

---
+++ dorm_text/src/main/java/edu/fdzc/dorm_text/entity/Student.java
@@ -0,0 +1,97 @@
+package com.example.wym.entity;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String id;
+    private String name;
+    private String id_card;
+    private String password;
+    private String gender;
+    private String department;
+    private String major;
+    private Integer grade;
+    private Integer clazz; // 避免使用 class 作为变量名，Java 关键字冲突
+    private String phone;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId_card() {
+        return id_card;
+    }
+
+    public void setId_card(String id_card) {
+        this.id_card = id_card;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+
+    public Integer getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(Integer clazz) {
+        this.clazz = clazz;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
